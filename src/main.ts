f1357463de7411d3d0c9c61ca45f0d9eebb59bf1
DaisukeMiyazaki <aubozumom@icloud.com>
2023-12-27 17:27:02 +0900

    feat: refactored for better readability and multiple file selects

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { App, Editor, EditorPosition, Notice, Plugin, PluginSettingTab, Setting, TextComponent, setIcon } from 'obsidian';
-import { Client } from 'minio-es';
 import { moment } from 'obsidian';
 import mime from 'mime';
 import { t } from './i18n';
+
+import * as Minio from 'minio';
+
+/* ******** */
+/* types */
 
 interface MinioPluginSettings {
 	accessKey: string;
@@ -11,10 +15,10 @@ interface MinioPluginSettings {
 	bucket: string;
 	endpoint: string;
 	port: number;
-	useSSL: boolean;
-	imgPreview: boolean;
-	videoPreview: boolean;
-	audioPreview: boolean;
+	isUseSSL: boolean;
+	isImgPreview: boolean;
+	isVideoPreview: boolean;
+	isAudioPreview: boolean;
 	docsPreview: string;
 	nameRule: string;
 	pathRule: string;
@@ -27,14 +31,16 @@ const DEFAULT_SETTINGS: MinioPluginSettings = {
 	endpoint: '',
 	port: 443,
 	bucket: '',
-	useSSL: true,
-	imgPreview: true,
-	videoPreview: true,
-	audioPreview: true,
+	isUseSSL: true,
+	isImgPreview: true,
+	isVideoPreview: true,
+	isAudioPreview: true,
 	docsPreview: '',
 	nameRule: 'local',
 	pathRule: 'root',
 }
+
+/* ******** */
 
 export default class MinioUploaderPlugin extends Plugin {
 	settings: MinioPluginSettings;
@@ -42,14 +48,6 @@ export default class MinioUploaderPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This adds a simple command that can be triggered anywhere
-		// this.addCommand({
-		// 	id: 'open-sample-modal-simple',
-		// 	name: 'Open sample modal (simple)',
-		// 	callback: () => {
-		// 		new SampleModal(this.app).open();
-		// 	}
-		// });
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'minio-uploader',
@@ -60,37 +58,42 @@ export default class MinioUploaderPlugin extends Plugin {
 				input.setAttribute('type', 'file')
 				input.setAttribute('accept', 'image/*,video/*,.doc,.docx,.pdf,.pptx,.xlsx,.xls')
 				input.onchange = async (event: Event) => {
-					const file = (event.target as any)?.files[0]
+					const files = (event.target as any)?.files
+					for (const file of files) {
+						const { endpoint, port, isUseSSL, bucket } = this.settings
+						const host = `http${isUseSSL ? 's' : ''}://${endpoint}${port === 443 || port === 80 ? '' : ':' + port}`
+						let original_text = `[${t('Uploading')}：0%](${file.name})\n`;
+						editor.replaceSelection(original_text);
 
-					const { endpoint, port, useSSL, bucket } = this.settings
-					const host = `http${useSSL ? 's' : ''}://${endpoint}${port === 443 || port === 80 ? '' : ':' + port}`
-					let replaceText = `[${t('Uploading')}：0%](${file.name})\n`;
-					editor.replaceSelection(replaceText);
-
-					const objectName = await this.minioUploader(file, (process) => {
-						const replaceText2 = `[${t('Uploading')}：${process}%](${file.name})`;
-						this.replaceText(editor, replaceText, replaceText2)
-						replaceText = replaceText2
-					})
-					const url = `${host}/${bucket}/${objectName}`
-					this.replaceText(editor, replaceText, this.wrapFileDependingOnType(this.getFileType(file), url, file.name))
+						const objectName = await this.minioUploader(file, (process) => {
+							const _t = `[${t('Uploading')}：${process}%](${file.name})`;
+							this.replaceText(editor, original_text, _t)
+							original_text = _t
+						})
+						const url = `${host}/${bucket}/${objectName}`
+						this.replaceText(editor, original_text, this.wrapFileDependingOnType(this.getFileType(file), url, file.name))
+					}
 				}
 				input.click()
 			}
 		});
-
+		
+		
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new MinioSettingTab(this.app, this));
-
+		
 		this.registerEvent(
 			this.app.workspace.on("editor-paste", this.handleUploader.bind(this))
-		);
-		this.registerEvent(
-			this.app.workspace.on("editor-drop", this.handleUploader.bind(this))
-		);
-	}
+			);
+			this.registerEvent(
+				this.app.workspace.on("editor-drop", this.handleUploader.bind(this))
+				);
+			}
+			
+	onunload() {}
 
-	getFileType(file: File) {
+	getFileType(file: File): string {
+		/* ファイルのタイプを返す */
 		const imageType = /image.*/;
 		const videoType = /video.*/;
 		const audioType = /audio.*/;
@@ -113,36 +116,42 @@ export default class MinioUploaderPlugin extends Plugin {
 		if (evt.defaultPrevented) {
 			return;
 		}
-		let file: any = null;
+		let files : FileList | undefined = undefined;
 
 		// figure out what kind of event we're handling
 		switch (evt.type) {
 			case "paste":
-				file = (evt as ClipboardEvent).clipboardData?.files[0];
+				files = (evt as ClipboardEvent).clipboardData?.files;
 				break;
 			case "drop":
-				file = (evt as DragEvent).dataTransfer?.files[0];
+				files = (evt as DragEvent).dataTransfer?.files;
 		}
+		if (!files) return;
 
-		if (!file || file && !this.getFileType(file)) return;
+		for (const file of Array.from(files)) {
 
-		evt.preventDefault();
-		const { endpoint, port, useSSL, bucket } = this.settings
-		const host = `http${useSSL ? 's' : ''}://${endpoint}${port === 443 || port === 80 ? '' : ':' + port}`
-		let replaceText = `[${t('Uploading')}：0%](${file.name})\n`;
-		editor.replaceSelection(replaceText);
+			if (!file || file && !this.getFileType(file)) continue;
 
-		const objectName = await this.minioUploader(file, (process) => {
-			const replaceText2 = `[${t('Uploading')}：${process}%](${file.name})`;
-			this.replaceText(editor, replaceText, replaceText2)
-			replaceText = replaceText2
-		})
-		const url = `${host}/${bucket}/${objectName}`
-		this.replaceText(editor, replaceText, this.wrapFileDependingOnType(this.getFileType(file), url, file.name))
+			evt.preventDefault();
+			const { endpoint, port, bucket } = this.settings
+			const host = `http://${endpoint}${':' + port}`
+			new Notice('Uploading：' + process + '%');
+			let replaceText = `[${t('Uploading')}：0%](${file.name})\n`;
+			editor.replaceSelection(replaceText);
+
+			const objectName = await this.minioUploader(file, (process) => {
+				new Notice('Uploading：' + process + '%');
+				const _t = `[${t('Uploading')}：${process}%](${file?.name})`;
+				this.replaceText(editor, replaceText, _t)
+				replaceText = _t
+			})
+			const url = `${host}/${bucket}/${objectName}`
+			this.replaceText(editor, replaceText, this.wrapFileDependingOnType(this.getFileType(file), url, file.name))
+		}
 	}
 
-	genObjectName (file: File) {
-		let objectName = ''
+	getObjectName (file: File): string {
+		let objectName = 'test'
 		switch (this.settings.pathRule) {
 			case 'root':
 				objectName = ''
@@ -173,50 +182,6 @@ export default class MinioUploaderPlugin extends Plugin {
 		return objectName
 	}
 
-	minioUploader(file: File, progress?: (count: number) => void): Promise<string> {
-		return new Promise((resolve, reject) => {
-			try {
-				const minioClient = new Client({
-					endPoint: this.settings.endpoint,
-					port: this.settings.port,
-					useSSL: this.settings.useSSL,
-					region: this.settings.region,
-					accessKey: this.settings.accessKey,
-					secretKey: this.settings.secretKey
-				})
-
-				const objectName = this.genObjectName(file)
-				minioClient.presignedPutObject(this.settings.bucket, objectName, 1 * 60 * 60).then(presignedUrl => {
-					const xhr = new XMLHttpRequest();
-					xhr.upload.addEventListener("progress", (progressEvent) => {
-						if (progress) progress(Math.round((progressEvent.loaded / progressEvent.total) * 100))
-					}, false)
-					xhr.onreadystatechange = function () {
-						if (xhr.readyState === 4) {
-							if (xhr.status === 200) {
-								resolve(objectName)
-							} else {
-								console.error('xhr', xhr)
-								reject(xhr.status)
-								new Notice('Error: upload failed.' + xhr.status);
-							}
-						}
-					};
-					xhr.open("PUT", presignedUrl, true);
-					const va = mime.getType(objectName.substring(objectName.lastIndexOf('.'))) as string
-					xhr.setRequestHeader('Content-Type', va);
-
-					xhr.send(file);
-				}).catch(err => {
-					reject(err)
-					new Notice('Error: upload failed.' + err.message);
-				})
-			} catch (err) {
-				new Notice('Error: ' + err.message);
-			}
-		})
-	}
-
 	private replaceText(
 		editor: Editor,
 		target: string,
@@ -241,13 +206,13 @@ export default class MinioUploaderPlugin extends Plugin {
 		}
 	}
 
-	wrapFileDependingOnType(type: string, url: string, name: string) {
+	wrapFileDependingOnType(type: string, url: string, name: string): string {
 		if (type === 'image') {
-			return `${this.settings.imgPreview ? '!' : ''}[${name}](${url})\n`
+			return `${this.settings.isImgPreview ? '!' : ''}[${name}](${url})\n`
 		} else if (type === 'video') {
-			return `${this.settings.videoPreview ? `<video src="${url}" controls></video>` : `[${name}](${url})`}\n`;
+			return `${this.settings.isVideoPreview ? `<video src="${url}" controls></video>` : `[${name}](${url})`}\n`;
 		} else if (type === 'audio') {
-			return `${this.settings.audioPreview ? `<audio src="${url}" controls></audio>` : `[${name}](${url})`}\n`;
+			return `${this.settings.isAudioPreview ? `<audio src="${url}" controls></audio>` : `[${name}](${url})`}\n`;
 		} else if (type === 'doc') {
 			return `\n${this.settings.docsPreview ? `<iframe frameborder=0 border=0 width=100% height=800
 			src="${this.settings.docsPreview}${url}">
@@ -257,9 +222,6 @@ export default class MinioUploaderPlugin extends Plugin {
 		}
 	}
 
-	onunload() {
-
-	}
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -268,23 +230,63 @@ export default class MinioUploaderPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+	
+	
+	minioUploader(file: File, progress?: (count: number) => void): Promise<string> {	
+		return new Promise((resolve, reject) => {
+			try {
+				const minioClient = new Minio.Client({
+						endPoint: this.settings.endpoint,
+						port: this.settings.port,
+						accessKey: this.settings.accessKey,
+						secretKey: this.settings.secretKey,
+						useSSL: this.settings.isUseSSL,
+					});
+
+				const objectName = this.getObjectName(file)
+				new Notice('Uploading：' + 0 + '%');
+				new Notice(this.settings.bucket);
+				new Notice(objectName);
+				new Notice(file.name);
+				new Notice(file.type);
+				new Notice(this.settings.port.toString());
+				new Notice(this.settings.endpoint);
+				minioClient.presignedPutObject(this.settings.bucket, objectName, 1 * 60 * 60).then(presignedUrl => {
+					const xhr = new XMLHttpRequest();
+					new Notice('Uploading：' + 0 + '%');
+					xhr.upload.addEventListener("progress", (progressEvent) => {
+						if (progress) progress(Math.round((progressEvent.loaded / progressEvent.total) * 100))
+					}, false)
+					xhr.onreadystatechange = function () {
+						if (xhr.readyState === 4) {
+							if (xhr.status === 200) {
+								resolve(objectName)
+							} else {
+								console.error('xhr', xhr)
+								reject(xhr.status)
+								new Notice('Error: upload failed.' + xhr.status);
+							}
+						}
+					};
+					xhr.open("PUT", presignedUrl, true);
+					const va = mime.getType(objectName.substring(objectName.lastIndexOf('.'))) as string
+					xhr.setRequestHeader('Content-Type', va);
+
+					xhr.send(file);
+				}).catch((err) => {
+					console.error('presignedUrl', err)
+					reject(err)
+					new Notice('Error: upload failed.' + err);
+				})
+			} catch (error) {
+				new Notice('Error: upload failed.' + error);
+				reject(error)	
+			}
+		});
+	}
 }
 
-// class SampleModal extends Modal {
-// 	constructor(app: App) {
-// 		super(app);
-// 	}
-
-// 	onOpen() {
-// 		const { contentEl } = this;
-// 		contentEl.setText('Woah!');
-// 	}
-
-// 	onClose() {
-// 		const { contentEl } = this;
-// 		contentEl.empty();
-// 	}
-// }
+/* helper functions */
 
 const wrapTextWithPasswordHide = (text: TextComponent) => {
 	const hider = text.inputEl.insertAdjacentElement("beforebegin", createSpan());
@@ -388,9 +390,9 @@ class MinioSettingTab extends PluginSettingTab {
 		new Setting(containerEl)
 			.setName('SSL')
 			.addToggle(text => text
-				.setValue(this.plugin.settings.useSSL)
+				.setValue(this.plugin.settings.isUseSSL)
 				.onChange(async (value) => {
-					this.plugin.settings.useSSL = value;
+					this.plugin.settings.isUseSSL = value;
 					await this.plugin.saveSettings();
 				}));
 		containerEl.createEl("h3", { text: t("Object rules") });
@@ -427,27 +429,27 @@ class MinioSettingTab extends PluginSettingTab {
 			.setName(t('Image preview'))
 			.setDesc(t('Image preview description'))
 			.addToggle(text => text
-				.setValue(this.plugin.settings.imgPreview)
+				.setValue(this.plugin.settings.isImgPreview)
 				.onChange(async (value) => {
-					this.plugin.settings.imgPreview = value;
+					this.plugin.settings.isImgPreview = value;
 					await this.plugin.saveSettings();
 				}));
 		new Setting(containerEl)
 			.setName(t('Video preview'))
 			.setDesc(t('Video preview description'))
 			.addToggle(text => text
-				.setValue(this.plugin.settings.videoPreview)
+				.setValue(this.plugin.settings.isVideoPreview)
 				.onChange(async (value) => {
-					this.plugin.settings.videoPreview = value;
+					this.plugin.settings.isVideoPreview = value;
 					await this.plugin.saveSettings();
 				}));
 		new Setting(containerEl)
 			.setName(t('Audio preview'))
 			.setDesc(t('Audio preview description'))
 			.addToggle(text => text
-				.setValue(this.plugin.settings.audioPreview)
+				.setValue(this.plugin.settings.isAudioPreview)
 				.onChange(async (value) => {
-					this.plugin.settings.audioPreview = value;
+					this.plugin.settings.isAudioPreview = value;
 					await this.plugin.saveSettings();
 				}));
 		new Setting(containerEl)
